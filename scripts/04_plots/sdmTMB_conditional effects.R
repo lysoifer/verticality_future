06d6ffbfd27365e2e6f1217175af80185584e08d
Lydia Soifer <lysoifer@gmail.com>
2024-08-09 10:31:27 -0400

    conditional effect plots

--- scripts/04_plots/sdmTMB_conditional effects.R
+++ scripts/04_plots/sdmTMB_conditional effects.R
@@ -1,123 +1,70 @@
 library(sdmTMB)
 library(foreach)
 library(doParallel)
+library(visreg)
+library(ggeffects)
 
-load("results/sdmTMB_models/amphibians_meanvert.RData")
-visreg(m.final, "tmax_warm")
+mods = list.files("results/sdmTMB_models/", pattern = ".RData", full.names = T)
 
-dat = m.final$data
-dat %>% 
-  mutate(tt = ifelse(abs(y) < 23.5, "Tropical", "Temperate")) %>% 
-  ggplot(aes(x = vert.mean.ses, y = rich, color = tt)) +
-  geom_point(, size = 0.05) +
-  geom_smooth()
-
-conditional_effects = function(m, random_int, ncore = 7, fpath) {
-  # m: sdmTMB model object
-  # random_int: name of variable used as a random intercept in the model
-  # ncore: number of cores for parallelization
-  # fpath: file path to save predictions to
-  
-  dat = m$data
-  terms = as.character(m$formula[[1]])[3]
-  terms = strsplit(terms, split = "\\+")[[1]]
-  terms = grep("\\(", terms, invert = T, value = T)
-  terms = str_trim(terms, side = "both")
-  
-  dat = dat[,which(colnames(dat) %in% terms)]
-  
-  # make matrix with median values
-  newdat = apply(dat, 2, median)
-  newdat = matrix(newdat, nrow = 100, ncol = length(newdat), byrow = T)
-  colnames(newdat) = colnames(dat)
-  
-  if(!is.null(random_int)){
-    randint = unique(m$data[,random_int])
-  }
-  
-  # replace variables one by one to get conditional predictions
-  cl = makeCluster(ncore)
-  registerDoParallel(cl)
-  cond_resp = foreach(i = 1:ncol(dat)) %dopar% {
-    var = seq(min(dat[,i]), max(dat[,i]), length.out = 100)
-    n = newdat
-    n[,i] = var
-    n = as.data.frame(n)
-    p = foreach(r = randint, .combine = "rbind") %do% {
-      n[,random_int] = rep(r, 100)
-      
-      # predict to newdata
-      pred = sdmTMB:::predict.sdmTMB(m, newdata = n, type = "response", re_form = NA, se_fit = F)
-    }
-    
-  }
-  stopCluster(cl)
-  names(cond_resp) = colnames(dat)
-  save(cond_resp, file = fpath)
+plot_cond2d = function(modpath) {
+  load(modpath)
+  p2d.ses = visreg2d(mod, xvar = "log_precip_dry", yvar = "tmax_warm", scale = "response", plot.type = "gg", data = mod$data)
+  p2d.ses2 = visreg2d(mod, xvar = "log_precip_dry", yvar = "tmin_cold", scale = "response", plot.type = "gg", data = mod$data)
+  return(list(p2d.ses, p2d.ses2))
 }
 
+plts = foreach(i = 1:length(mods)) %do% {
+ p = plot_cond2d(mods[i])
+ p
+}
+names(plts) = mods
 
-# Unscale the data --------------------------------------------------------
+row1 = wrap_elements(panel = textGrob(expression(bold("Birds")), rot = 90, gp = gpar(fonface = "bold")))
+row2 = wrap_elements(panel = textGrob(expression(bold("Mammals")), rot = 90, gp = gpar(fonface = "bold")))
+row3 = wrap_elements(panel = textGrob(expression(bold("Reptiles")), rot = 90, gp = gpar(fonface = "bold")))
+row4 = wrap_elements(panel = textGrob(expression(bold("Amphibians")), rot = 90, gp = gpar(fonface = "bold")))
 
-# amphibian forest only 50km resolution
-raw = read.csv("data/derivative_data/gridcell_data/env_forest/50_km/amph_comdat.csv")
+plts.meanvert = list(row1, b1 = plts[[3]][[1]], b2 = plts[[3]][[2]],
+                     row2, m1 = plts[[5]][[1]], m2 = plts[[5]][[2]],
+                     row3, r1 = plts[[7]][[1]], r2 = plts[[7]][[2]],
+                     row4, a1 = plts[[1]][[1]], a2 = plts[[1]][[2]])
 
-# remove cells with fewer than five species
-raw = raw %>% 
-  # subset to richness >= 5
-  filter(rich >= 5) %>% 
-  filter(!is.na(vert.mean.ses)) %>% 
-  drop_na()
+plot = wrap_plots(plts.meanvert) +
+  plot_layout(ncol = 3, nrow = 4, byrow = T, 
+              widths = c(0.1,1,1),
+              axis_titles = "collect_x") +
+  plot_annotation(title = "Mean Verticality", 
+                  tag_levels = list(c("", "A", "B", 
+                                 "", "C", "D",
+                                 "", "E", "F",
+                                 "", "G", "H"))) &
+  theme(legend.title = element_blank(),
+        plot.title = element_text(hjust = 0.5, face = "bold"),
+        plot.tag.position = c(0.02,0.98))
 
-# future env data
-env.f = rast(list.files(path = "data/derivative_data/resampled_env_rasters_50km/chelsa_future/2071_2100/ensemble/",
-                        pattern = ".tif", full.names = T))
-env.f = env.f[[2:6]]
-
-dat.f = raw %>% 
-  dplyr::select(x,y,canopy_height, veg_den, clim_velocity, ecoregion)
-
-env.f = terra::extract(env.f, dat.f[,c('x', "y")], ID = F)
-
-dat.f = cbind(dat.f, env.f)
-dat.f = dat.f %>% rename(tmax_warm = max_temp_warm, tmin_cold = min_temp_cold)
-
-# remove NA values from future and present data
-raw = raw[which(!is.na(dat.f$precip_dry)),]
-dat.f = dat.f[which(!is.na(dat.f$precip_dry)),]
-
-dat.t = raw %>% 
-  mutate(log_clim_velocity = log10(clim_velocity),
-         log_tmin_cold = log10(tmin_cold + 50),
-         log_precip_dry = log10(precip_dry),
-         log_precip_wet = log10(precip_wet))
+png("figures/supp_figs/cond_effects_meanvert.png", width = 225, height = 300, res = 300, units = "mm")
+plot
+dev.off()
 
 
-ggplot(cond_resp[[3]]) +
-  geom_line(aes(x = tmax_warm, y = est, colour = ecoregion)) +
-  theme(legend.position = "none")
+plts.sesvert = list(row1, b1 = plts[[4]][[1]], b2 = plts[[4]][[2]],
+                     row2, m1 = plts[[6]][[1]], m2 = plts[[6]][[2]],
+                     row3, r1 = plts[[8]][[1]], r2 = plts[[8]][[2]],
+                     row4, a1 = plts[[2]][[1]], a2 = plts[[2]][[2]])
 
-##RESCALE X AXIS#
-ggplot(cond_resp[[7]]) +
-  geom_line(aes(x = 10^(log_precip_dry*), y = est, colour = ecoregion)) +
-  theme(legend.position = "none")
+plot = wrap_plots(plts.sesvert) +
+  plot_layout(ncol = 3, nrow = 4, byrow = T, 
+              widths = c(0.1,1,1),
+              axis_titles = "collect_x") +
+  plot_annotation(title = "SES Verticality", 
+                  tag_levels = list(c("", "A", "B", 
+                                      "", "C", "D",
+                                      "", "E", "F",
+                                      "", "G", "H"))) &
+  theme(legend.title = element_blank(),
+        plot.title = element_text(hjust = 0.5, face = "bold"),
+        plot.tag.position = c(0.02,0.98))
 
-ggplot(cond_resp[[1]]) +
-  geom_line(aes(x = canopy_height, y = est, colour = ecoregion)) +
-  theme(legend.position = "none")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+png("figures/supp_figs/cond_effects_sesvert.png", width = 225, height = 300, res = 300, units = "mm")
+plot
+dev.off()
