241f2cb426a2ebbe59c4b57c00f615a92b375240
lysoifer <lysoifer@gmail.com>
2025-02-09 17:37:32 -0500

    update fig3 coef plot

--- scripts/04_plots/fig3_coef_plot.R
+++ scripts/04_plots/fig3_coef_plot.R
@@ -6,159 +6,91 @@ library(ggh4x)
 library(spatialreg)
 library(sdmTMB)
 
-# VERTMEAN ----------------------------------------------------------------
 
-# ADD variable importance weights to the graphs ##
+# SES Verticality Model Coefficients --------------------------------------
 
-load("results/sdmTMB_models/amphibians_meanvert.RData")
-amph.mod = mod
+amph = readRDS("results/sdmTMB_models2/amphibians_sesvert.rds")
+birds = readRDS("results/sdmTMB_models2/birds_sesvert.rds")
+mammals = readRDS("results/sdmTMB_models2/mammals_sesvert.rds")
+repts = readRDS("results/sdmTMB_models2/reptiles_sesvert.rds")
 
-load("results/sdmTMB_models/mammals_meanvert.RData")
-mammals.mod = mod
+mods = list(amph$bestmod, birds$bestmod, mammals$bestmod, repts$bestmod)
+mods = lapply(mods, tidy, effects = "fixed", conf.int = T)
+mods[[1]]$taxa = "Amphibians"
+mods[[2]]$taxa = "Birds"
+mods[[3]]$taxa = "Mammals"
+mods[[4]]$taxa = "Reptiles"
 
-load("results/sdmTMB_models/reptiles_meanvert.RData")
-rept.mod = mod
-
-load("results/sdmTMB_models/birds_meanvert.RData")
-birds.mod = mod
-
-# * - get coefficients with 95% CI for each class -----------------------------------------
-amph.coefs = tidy(amph.mod, conf.int = T, conf.level = 0.95, effects = "fixed")
-amph.coefs$class = "Amphibians"
-
-mammals.coefs = tidy(mammals.mod, conf.int = T, conf.level = 0.95, effects = "fixed")
-mammals.coefs$class = "Mammals"
-
-reptiles.coefs = tidy(rept.mod, conf.int = T, conf.level = 0.95, effects = "fixed")
-reptiles.coefs$class = "Reptiles"
-
-birds.coefs = tidy(birds.mod, conf.int = T, conf.level = 0.95, effects = "fixed")
-birds.coefs$class = "Birds"
-
-
-meanvert.coefs = bind_rows(amph.coefs, mammals.coefs, reptiles.coefs, birds.coefs)%>% 
-  mutate(class = factor(class, levels = c("Birds", "Mammals", "Reptiles", "Amphibians")),
-         type = "Mean Verticality",
-         sig = ifelse(conf.low*conf.high > 0, "sig", "notsig"))
-
-
-# * - plot --------------------------------------------------------------------
-
-meanvert.coefs %>% 
-  filter(term != "(Intercept)") %>% 
-  ggplot() +
-  geom_pointrange(aes(x = estimate, y = reorder(term, estimate), xmin = conf.low, xmax = conf.high)) +
-  geom_vline(xintercept = 0, linetype = "dashed") +
-  #geom_text(data = meanvert.r2, aes(label = paste0("R\u00b2 = ", r2)), x = 0.042, y = 12, inherit.aes = F, hjust = 1, vjust = 0) +
-  #annotate(geom = "text", x = -0.005, y = 7, label = paste0("R\u00b2 = ", r2)) +
-  #scale_color_manual(values = c("black", "red3")) +
-  #scale_y_discrete("") +
-  #coord_cartesian(clip = "off", ylim = c(0,13)) +
-  facet_wrap(facets = ~class) +
-  ggtitle("Mean Verticality") +
-  theme(plot.title = element_text(hjust = 0.5),
-        panel.background = element_rect(color = "black", fill = "white"),
-        panel.grid = element_line(color = "gray90"),
-        strip.background = element_rect(color = "black"))
-
-# SES VERTMEAN ----------------------------------------------------------------
-
-load("results/sdmTMB_models/amphibians_sesvert.RData")
-amph.mod = mod
-
-load("results/sdmTMB_models/mammals_sesvert.RData")
-mammals.mod = mod
-
-load("results/sdmTMB_models/reptiles_sesvert.RData")
-rept.mod = mod
-
-load("results/sdmTMB_models/birds_sesvert.RData")
-birds.mod = mod
-
-
-
-# * - get coefficients for each class -----------------------------------------
-amph.coefs = tidy(amph.mod, conf.int = T, conf.level = 0.95, effects = "fixed")
-amph.coefs$class = "Amphibians"
-
-mammals.coefs = tidy(mammals.mod, conf.int = T, conf.level = 0.95, effects = "fixed")
-mammals.coefs$class = "Mammals"
-
-reptiles.coefs = tidy(rept.mod, conf.int = T, conf.level = 0.95, effects = "fixed")
-reptiles.coefs$class = "Reptiles"
-
-birds.coefs = tidy(birds.mod, conf.int = T, conf.level = 0.95, effects = "fixed")
-birds.coefs$class = "Birds"
-
-
-sesvert.coefs = bind_rows(amph.coefs, mammals.coefs, reptiles.coefs, birds.coefs)%>% 
-  mutate(class = factor(class, levels = c("Birds", "Mammals", "Reptiles", "Amphibians")),
-         type = "SES Mean Verticality",
-         sig = ifelse(conf.low*conf.high > 0, "sig", "notsig"))
-
-
-
-# * - plot --------------------------------------------------------------------
-
-sesvert.coefs %>% 
+mods = bind_rows(mods)
+mods = mods %>% 
   filter(term != "(Intercept)") %>%
-  ggplot(aes(x = estimate, y = reorder(term, estimate), xmin = conf.low, xmax = conf.high)) +
-  geom_pointrange() +
-  geom_vline(xintercept = 0, linetype = "dashed") +
-  scale_y_discrete("") +
-  facet_wrap(facets = ~class) +
-  ggtitle("SES Mean Verticality") +
-  theme(plot.title = element_text(hjust = 0.5),
-        panel.background = element_rect(color = "black", fill = "white"),
-        panel.grid = element_line(color = "gray90"),
-        strip.background = element_rect(color = "black"))
-
-
-# plot meanvert and sesmeanvert -------------------------------------------
-
-coefs = bind_rows(sesvert.coefs, meanvert.coefs) %>% 
-  filter(term != "(Intercept)") %>%
-  mutate(term = factor(term, levels = c(
-    "log_clim_velocity", "canopy_height", "veg_den",
-    "precip_wet", "log_precip_dry",
-    "I(tmax_warm^2)", "tmax_warm", "tmin_cold")),
-    class = factor(class, levels = c("Birds", "Mammals", "Reptiles", "Amphibians")))
-
-coefs = bind_rows(sesvert.coefs, meanvert.coefs) %>% 
-  filter(term != "(Intercept)") %>%
-  mutate(term = case_when(term == "log_clim_velocity" ~ "log(climate velocity)",
-                          term == "canopy_height" ~ "Canopy height",
+  mutate(taxa = factor(taxa, levels = c("Birds", "Mammals", "Reptiles", "Amphibians")),
+         term = case_when(term == "canopy_height:tmin_cold" ~ "canopy height:Tmin[cold]",
+                          term == "canopy_height:tmax_warm" ~ "canopy height:Tmax[warm]",
+                          term == "canopy_height:log_precip_dry" ~ "canopy height:log(Precip[dry]",
+                          term == "precip_warm:canopy_height" ~ "Precip[warm]:canopy height",
+                          term == "log_clim_velocity" ~ "log(climate velocity)", 
                           term == "veg_den" ~ "Vegetation density",
-                          term == "precip_wet" ~ "Wet season precip",
-                          term == "log_precip_dry" ~ "log(dry season precip)",
-                          term == "I(tmax_warm^2)" ~ "(Max temp warmest month)\u00b2",
-                          term == "tmax_warm" ~ "Max temp warmest month",
-                          term == "tmin_cold" ~ "Min temp coldest month"),
-         term = factor(term, levels = c(
-    "log(climate velocity)", "Canopy height", "Vegetation density",
-    "Wet season precip", "log(dry season precip)",
-    "(Max temp warmest month)\u00b2", "Max temp warmest month", "Min temp coldest month")),
-    class = factor(class, levels = c("Birds", "Mammals", "Reptiles", "Amphibians")))
+                          term == "canopy_height" ~ "Canopy height",
+                          term == "I(canopy_height^2)" ~ "(Canopy height)\u00b2",
+                          term == "I(precip_warm^2)" ~ "(Precip[warm])\u00b2",
+                          term == "precip_warm" ~ "Precip[warm]",
+                          term == "precip_wet" ~ "Precip[wet]",
+                          term == "I(log_precip_dry^2)" ~ "(log(Precip[dry]))\u00b2",
+                          term == "log_precip_dry" ~ "log(Precip[dry])",
+                          term == "I(tmax_warm^2)" ~ "(Tmax[warm])\u00b2",
+                          term == "tmax_warm" ~ "Tmax[warm]",
+                          term == "I(tmin_cold^2)" ~ "(Tmin[cold])\u00b2",
+                          term == "tmin_cold" ~ "Tmin[cold]"),
+        term = factor(term, levels = c(
+          "canopy height:Tmin[cold]",
+          "canopy height:Tmax[warm]",
+          "canopy height:log(Precip[dry]",
+          "Precip[warm]:canopy height",
+          "log(climate velocity)",
+          "Vegetation density",
+          "(Canopy height)\u00b2",
+          "Canopy height",
+          "(Precip[warm])\u00b2",
+          "Precip[warm]",
+          "Precip[wet]",
+          "(log(Precip[dry]))\u00b2",
+          "log(Precip[dry])",
+          "(Tmax[warm])\u00b2",
+          "Tmax[warm]",
+          "(Tmin[cold])\u00b2",
+          "Tmin[cold]")),
+        sig = ifelse(conf.low*conf.high > 0, "sig", "notsig"))
+
+
 
 
 library(ggh4x)
-coef.plt = coefs %>%
+coef.plt = mods %>%
   ggplot(aes(x = estimate, y = term, xmin = conf.low, xmax = conf.high, color = sig)) +
   geom_pointrange(size = 0.15) +
   geom_vline(xintercept = 0, linetype = "dashed") +
-  #geom_text(data = r2, aes(label = paste0("R\u00b2 = ", r2), x = Estimate), y = 2, inherit.aes = F, hjust = 1, vjust = 0) +
-  #geom_text(data = r2, aes(label = paste0("R\u00b2 trend = ", r2.trend), x = Estimate), y = 0.9, inherit.aes = F, hjust = 1, vjust = 0) +
-  #scale_color_manual(values = c("black", "red3")) +
-  #scale_size_continuous(range = c(0.02,0.5)) +
-  scale_y_discrete("") +
+  scale_y_discrete("",
+                   labels = c("canopy height:Tmin[cold]" = expression("Canopy height:Tmin"[cold]),
+                              "canopy height:Tmax[warm]" = expression("Canopy height:Tmax"[warm]),
+                              "canopy height:log(Precip[dry]" = expression("Canopy height:log(Precip"["dry"]*")"),
+                              "Precip[warm]:canopy height" = expression("Precip"[warm]*":Canopy height"),
+                              "(Precip[warm])\u00b2" = expression("(Precip"["warm"]*")\u00b2"),
+                              "Precip[warm]" = expression("Precip"[warm]),
+                              "Precip[wet]" = expression("Precip"[wet]),
+                              "(log(Precip[dry]))\u00b2" = expression("(log(Precip"["dry"]*"))\u00b2"),
+                              "log(Precip[dry])" = expression("log(Precip"["dry"]*")"),
+                              "(Tmax[warm])\u00b2" = expression("(Tmax"["warm"]*")\u00b2"),
+                              "Tmax[warm]" = expression("Tmax"[warm]),
+                              "(Tmin[cold])\u00b2" = expression("(Tmin"["cold"]*")\u00b2"),
+                              "Tmin[cold]" = expression("Tmin"[cold]))) +
   #scale_x_continuous("Effect size of standardized predictors") +
   scale_color_manual(values = c("black", "red3")) +
-  facet_grid2(rows = vars(type), cols = vars(class), scales = "free_x", independent = "x") +
-  scale_x_facet(ROW == 1, limits = c(-0.08, 0.18)) +
-  scale_x_facet(ROW == 2, limits = c(-0.2, 0.7)) +
+  facet_wrap(vars(taxa)) +
+  # facet_grid2(rows = vars(type), cols = vars(class), scales = "free_x", independent = "x") +
+  # scale_x_facet(ROW == 1, limits = c(-0.08, 0.18)) +
+  # scale_x_facet(ROW == 2, limits = c(-0.2, 0.7)) +
   labs(x = "Effect size of standardized predictors") +
-  #facet_grid(class ~ type, scales = "free") +
-  #ggtitle("SES Mean Verticality") +
   theme(plot.title = element_text(hjust = 0.5),
         panel.background = element_rect(color = "black", fill = "white"),
         panel.grid.major = element_line(color = "gray90"),
@@ -171,6 +103,6 @@ coef.plt = coefs %>%
         legend.title = element_text(hjust = 0.5),
         axis.text.x = element_text(size = 6))
 
-png("figures/main_figs/fig3_sdmTMB_coefs.png", width = 180, height = 98, res = 600, units = "mm")
+png("figures/main_figs/fig3/fig3_sdmTMB_coefs.png", width = 180, height = 180, res = 600, units = "mm")
 coef.plt
 dev.off()
